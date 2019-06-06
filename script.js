ecfd7a6d1553c10e068af82506b1e030e9a8168a
Arno <arno.richter+github@gmail.com>
2019-06-06 23:03:56 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 var init = function() {
 	alert('It works!');
+	
+	fetch('/data.json')
+	.then(function(response) {
+		return response.json();
+	})
+	.then(function(myJson) {
+		console.log(JSON.stringify(myJson));
+	});
 }
 
 document.addEventListener('DOMContentLoaded', init);
